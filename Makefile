717c59d5c2a480a48c0a72d3b0d95b0606047bb5
Aruã Sousa <>
2019-04-10 11:35:13 -0300

    melhoria na geraçãod e arvores e legibilidade

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@ edit : src/main.o src/cenario.o src/figuras.o \
 
 main.o : main.c macros.h
 	 cc -c main.c
-cenario.o : cenario.c cenario.h
+cenario.o : cenario.c cenario.h macros.h texturas.h
 	cc -c cenario.c
-figuras.o : figuras.c figuras.h
+figuras.o : figuras.c figuras.h macros.h texturas.h logica.h
 	cc -c figuras.c
 iluminacao.o : iluminacao.c iluminacao.h
 	cc -c iluminacao.c
-logica.o : logica.c logica.h
+logica.o : logica.c logica.h macros.h
 	cc -c logica.c
 texturas.o : texturas.c texturas.h
 	cc -c texturas.c
--- src/cenario.c
+++ src/cenario.c
@@ -19,30 +19,44 @@
 
 static GLUquadricObj *obj;
 
-float tx1[NUM_TREES/2], tx2[NUM_TREES/2], ty1[NUM_TREES/2], ty2[NUM_TREES/2];
+//float tx1[NUM_TREES/2], tx2[NUM_TREES/2], ty1[NUM_TREES/2], ty2[NUM_TREES/2];
+float tx[NUM_TREES], ty[NUM_TREES];
 
-static inline double sig(double x, int i) {
-    return i == 1 ? x : -x;
+static inline double sig(double x, double i) {
+    return i >= 0 ? x : -x;
 }
 
 static inline double rndSig(double x) {
     return sig(x, rand()%2 + 1);
 }
 
+static inline double rand_pos() {
+    return ((double)rand() / (double)RAND_MAX)*2.0f - 1;
+}
+
+/*
 #define auxInit(id) macrofy(\
     range(i, 0, NUM_TREES/2){ \
         ty##id[i] = rndSig(((double)rand() / (double)RAND_MAX)*160); \
-        xRange = (powf(ty##id[i],2.0f)/8.0f)/100.0f; \
+        xRange = (powf(ty##id[i],2.0f)/800.0f); \
         tx##id[i] = sig((((double)rand() / (double)RAND_MAX))*(xRange+25) + 10, id); \
         dprintf("%f, %f\n", tx##id[i], ty##id[i]); \
     })
-
+*/
 void initCenario(){
     obj = gluNewQuadric();
     gluQuadricDrawStyle(obj,GLU_FILL);
     float xRange;
-	auxInit(1);
-    auxInit(2);
+    //auxInit(1);
+    //auxInit(2);
+    float raw_x; \
+    range(i, 0, NUM_TREES){
+        ty[i] = rand_pos()*160.0f;
+        xRange = (powf(ty[i],2.0f)/800.0f);
+	raw_x = rand_pos();
+        tx[i] = raw_x*(xRange+25) + sig(10.0f, raw_x);
+        dprintf("%f, %f\n", tx[i], ty[i]);
+    }
 }
 
 void drawBranches(float x, float y){
@@ -65,71 +79,73 @@ void drawTrunk(float x, float y){
     });
 }
 
+void drawGround(){
+    transform({//desenha chão
+        glColor3f(0,0.6,0);
+        glTranslatef(0,-355,0);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glEnable ( GL_TEXTURE_2D );
+        glBindTexture(GL_TEXTURE_2D, texturesID[GRAMA]);
+        draw(GL_QUADS, {
+            glNormal3f(-1.0, 0.0, 0.0);
+            glTexCoord2f(0.0f, 0.0f); glVertex3f(-3200.0f, 0.0f, -3200.0f);
+            glTexCoord2f(0.0f, 7.0f); glVertex3f(-3200.0f, 0.0f, 3200.0f);
+            glTexCoord2f(7.0f, 7.0f); glVertex3f(3200.0f, 0.0f, 3200.0f);
+            glTexCoord2f(7.0f, 0.0f); glVertex3f(3200.0f, 0.0f, -3200.0f);
+        });
+        glDisable ( GL_TEXTURE_2D );
+    });
+}
+   
+void drawRiver(){
+    transform({//desenha rio
+        glColor3f(0,0,0);
+        glTranslatef(0,-345,0);
+        glScalef(0.5,0.003,8);
+        glEnable ( GL_TEXTURE_2D );
+        glBindTexture(GL_TEXTURE_2D, texturesID[RIO]);
+        glBegin(GL_QUADS);
+            glNormal3f(-1.0, 0.0, 0.0);
+            glTexCoord2f(0.0f, 0.0f); glVertex3f(-400.0f, 0.0f, -400.0f);
+            glTexCoord2f(0.0f, 10.0f); glVertex3f(-400.0f, 0.0f, 400.0f);
+            glTexCoord2f(1.0f, 10.0f); glVertex3f(400.0f, 0.0f, 400.0f);
+            glTexCoord2f(1.0f, 0.0f); glVertex3f(400.0f, 0.0f, -400.0f);
+        glEnd();
+	glDisable ( GL_TEXTURE_2D );
+    });
+}
+
+void drawSky(){
+    transform({//desenha ceu
+        glColor3f(1,1,1);
+        glTranslatef(0,-345,-3200);
+        glScalef(16,8,1);
+        glEnable ( GL_TEXTURE_2D );
+        glBindTexture(GL_TEXTURE_2D, texturesID[CEU]);
+        glBegin(GL_QUADS);
+            glNormal3f(-1.0, 0.0, 0.0);
+            glTexCoord2f(0.0f, 0.0f); glVertex3f(-400.0f, -200.0f, 0.0f);
+            glTexCoord2f(0.0f, 1.0f); glVertex3f(-400.0f, 600.0f, 0.0f);
+            glTexCoord2f(1.0f, 1.0f); glVertex3f(400.0f, 600.0f, 0.0f);
+            glTexCoord2f(1.0f, 0.0f); glVertex3f(400.0f, -200.0f, 0.0f);
+        glEnd();
+        glDisable ( GL_TEXTURE_2D );
+    });
+}
+
 void cenario(){
     transform({
-    	glColor3f(0.7,0.7,0.7);
-    	glDisable(GL_COLOR_MATERIAL);
-        transform({//desenha chão
-            glColor3f(0,0.6,0);
-            glTranslatef(0,-355,0);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glEnable ( GL_TEXTURE_2D );
-            glBindTexture(GL_TEXTURE_2D, texturesID[GRAMA]);
-            draw(GL_QUADS, {
-				glNormal3f(-1.0, 0.0, 0.0);
-				glTexCoord2f(0.0f, 0.0f); glVertex3f(-3200.0f, 0.0f, -3200.0f);
-				glTexCoord2f(0.0f, 7.0f); glVertex3f(-3200.0f, 0.0f, 3200.0f);
-				glTexCoord2f(7.0f, 7.0f); glVertex3f(3200.0f, 0.0f, 3200.0f);
-				glTexCoord2f(7.0f, 0.0f); glVertex3f(3200.0f, 0.0f, -3200.0f);
-			});
-			glDisable ( GL_TEXTURE_2D );
-        });
-        transform({//desenha rio
-            glColor3f(0,0,0);
-            glTranslatef(0,-345,0);
-            glScalef(0.5,0.003,8);
-            glEnable ( GL_TEXTURE_2D );
-            //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glBindTexture(GL_TEXTURE_2D, texturesID[RIO]);
-            glBegin(GL_QUADS);
-            
-				glNormal3f(-1.0, 0.0, 0.0);
-				glTexCoord2f(0.0f, 0.0f); glVertex3f(-400.0f, 0.0f, -400.0f);
-				glTexCoord2f(0.0f, 10.0f); glVertex3f(-400.0f, 0.0f, 400.0f);
-				glTexCoord2f(1.0f, 10.0f); glVertex3f(400.0f, 0.0f, 400.0f);
-				glTexCoord2f(1.0f, 0.0f); glVertex3f(400.0f, 0.0f, -400.0f);
-
-			glEnd();
-			glDisable ( GL_TEXTURE_2D );
-        });
+        glColor3f(0.7,0.7,0.7);
+        glDisable(GL_COLOR_MATERIAL);
+        drawGround();
+        drawRiver();
+        drawSky();
         glEnable(GL_COLOR_MATERIAL);
-        transform({//desenha ceu
-            glColor3f(1,1,1);
-            glTranslatef(0,-345,-3200);
-            glScalef(16,8,1);
-            glEnable ( GL_TEXTURE_2D );
-            //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glBindTexture(GL_TEXTURE_2D, texturesID[CEU]);
-            glBegin(GL_QUADS);
-            
-				glNormal3f(-1.0, 0.0, 0.0);
-				glTexCoord2f(0.0f, 0.0f); glVertex3f(-400.0f, -200.0f, 0.0f);
-				glTexCoord2f(0.0f, 1.0f); glVertex3f(-400.0f, 600.0f, 0.0f);
-				glTexCoord2f(1.0f, 1.0f); glVertex3f(400.0f, 600.0f, 0.0f);
-				glTexCoord2f(1.0f, 0.0f); glVertex3f(400.0f, -200.0f, 0.0f);
-
-			glEnd();
-			glDisable ( GL_TEXTURE_2D );
-        });
         
-        range(i, 0, NUM_TREES/2) {
-        	drawTrunk(tx1[i], ty1[i]);
-            drawBranches(tx1[i], ty1[i]);
-            drawTrunk(tx2[i], ty2[i]);
-            drawBranches(tx2[i], ty2[i]);
+        range(i, 0, NUM_TREES) {
+            drawTrunk(tx[i], ty[i]);
+            drawBranches(tx[i], ty[i]);
         }
     });
 }
